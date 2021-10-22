6c522dbb62fc9d6b236f24e09e6831c28cafa283
Takumikimura141899 <kimura141899@gmail.com>
2021-10-22 20:43:57 +0900

    [add] templateLiteral

---
+++ src/templateLiteral/templateLiteral.js
@@ -0,0 +1,7 @@
+var year = 2018;
+var yearMessage = "\u4ECA\u5E74\u306F" + year + "\u5E74\u3067\u3059";
+console.log(yearMessage);
+function doubleMessage(number) {
+    return number + "\u30922\u500D\u3059\u308B\u3068" + 2 * number + "\u306B\u306A\u308A\u307E\u3059";
+}
+console.log(doubleMessage(20));
---
+++ src/templateLiteral/templateLiteral.ts
@@ -0,0 +1,11 @@
+const year = 2018;
+
+const yearMessage = `今年は${year}年です`;
+
+console.log(yearMessage);
+
+function doubleMessage(number: number) {
+  return `${number}を2倍すると${2 * number}になります`;
+}
+
+console.log(doubleMessage(20));
