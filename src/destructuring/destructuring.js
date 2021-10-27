ce0dbe306a2dfde924b701d5a4ed41cf0e9926c2
Takumikimura141899 <kimura141899@gmail.com>
2021-10-27 12:15:10 +0900

    [add] destructuring

--- src/destructuring/destructuring.js
+++ src/destructuring/destructuring.js
@@ -6,3 +6,57 @@ var expense = {
 // const amount = expense.amount;
 var type = expense.type, amount = expense.amount;
 console.log(type, amount);
+var savedFile = {
+    extension: 'jpg',
+    name: 'profile',
+    size: 14040
+};
+function fileSummary(_a, _b) {
+    var name = _a.name, extension = _a.extension, size = _a.size;
+    var userName = _b.userName;
+    return userName + ": " + name + "." + extension + "\u306E\u5BB9\u91CF\u306F" + size + "\u3067\u3059\u3002";
+}
+console.log(fileSummary(savedFile, { userName: 'taku' }));
+var companies = ['Google', 'Facebook', 'Uber'];
+var name1 = companies[0], name2 = companies[1], name3 = companies[2];
+console.log(name1, name2, name3);
+var fruits = ['もも', 'ぶどう', 'メロン', 'りんご'];
+var fruit1 = fruits[0], fruit2 = fruits[1], rest = fruits.slice(2);
+console.log(fruit1, rest);
+var companies1 = [
+    { name: 'Google', location1: 'マウンテンビュー' },
+    { name: 'Facebook', location: 'メンロパーク' },
+    { name: 'Uber', location: 'サンフランシスコ' },
+];
+var location1 = companies1[0].location1;
+var Google = {
+    locations: ['マウンテンビュー', 'ニューヨーク', 'ロサンゼルス']
+};
+var first = Google.locations[0];
+console.log(first);
+var locs = Google.locations;
+var second = locs[1];
+console.log(second);
+var locations = Google.locations;
+console.log(locations);
+function signup(_a) {
+    var userName = _a.userName, password = _a.password, email = _a.email, birthday = _a.birthday, city = _a.city;
+    // ここでユーザーを作成してDBに保存する
+}
+var user1 = {
+    userName: 'myUserName',
+    password: 'myPassword',
+    email: 'myemail@example.com',
+    birthday: '1990/1/1',
+    city: '東京'
+};
+console.log(signup(user1));
+var points = [
+    [4, 5],
+    [10, 1],
+    [0, 40],
+];
+console.log(points.map(function (_a) {
+    var x = _a[0], y = _a[1];
+    return { x: x, y: y };
+}));
--- src/destructuring/destructuring.ts
+++ src/destructuring/destructuring.ts
@@ -9,3 +9,89 @@ const expense = {
 const { type, amount } = expense;
 
 console.log(type, amount);
+
+const savedFile = {
+  extension: 'jpg',
+  name: 'profile',
+  size: 14040,
+};
+
+function fileSummary(
+  {
+    name,
+    extension,
+    size,
+  }: {
+    extension: string;
+    name: string;
+    size: number;
+  },
+  { userName }: { userName: string }
+) {
+  return `${userName}: ${name}.${extension}の容量は${size}です。`;
+}
+
+console.log(fileSummary(savedFile, { userName: 'taku' }));
+
+const companies = ['Google', 'Facebook', 'Uber'];
+
+const [name1, name2, name3] = companies;
+
+console.log(name1, name2, name3);
+
+const fruits = ['もも', 'ぶどう', 'メロン', 'りんご'];
+
+const [fruit1, fruit2, ...rest] = fruits;
+console.log(fruit1, rest);
+
+const companies1 = [
+  { name: 'Google', location1: 'マウンテンビュー' },
+  { name: 'Facebook', location: 'メンロパーク' },
+  { name: 'Uber', location: 'サンフランシスコ' },
+];
+
+const [{ location1 }] = companies1;
+
+const Google = {
+  locations: ['マウンテンビュー', 'ニューヨーク', 'ロサンゼルス'],
+};
+
+const {
+  locations: [first],
+} = Google;
+
+console.log(first);
+
+const { locations: locs } = Google;
+const [, second] = locs;
+
+console.log(second);
+
+const { locations } = Google;
+console.log(locations);
+
+function signup({ userName, password, email, birthday, city }) {
+  // ここでユーザーを作成してDBに保存する
+}
+
+const user1 = {
+  userName: 'myUserName',
+  password: 'myPassword',
+  email: 'myemail@example.com',
+  birthday: '1990/1/1',
+  city: '東京',
+};
+
+console.log(signup(user1));
+
+const points = [
+  [4, 5],
+  [10, 1],
+  [0, 40],
+];
+
+console.log(
+  points.map(([x, y]) => {
+    return { x, y };
+  })
+);
