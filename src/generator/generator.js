d2310458a18bffcac80711c3031939462b6e42fd
Takumikimura141899 <kimura141899@gmail.com>
2021-10-31 08:28:08 +0900

    [add] generator

---
+++ src/generator/generator.js
@@ -0,0 +1,17 @@
+var x = 0;
+
+switch (x) {
+  case x === 2:
+    console.log('それは2です');
+    break;
+  case x === 1:
+    console.log('それは1です');
+    break;
+  case x === 0:
+    console.log('それは0です');
+    break;
+
+  default:
+    console.log('不明');
+    break;
+}
